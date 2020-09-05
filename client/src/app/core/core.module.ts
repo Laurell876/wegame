3bd7287d7d3dfefe4a5ec209a3a70b60288536ca
Laurell Seville <laurellseville2000@gmail.com>
2020-09-05 12:40:47 -0500

    showing alerts for errors is working

--- client/src/app/core/core.module.ts
+++ client/src/app/core/core.module.ts
@@ -6,18 +6,27 @@ import { MatToolbarModule } from '@angular/material/toolbar';
 import {MatButtonModule} from '@angular/material/button';
 import {MatIconModule} from '@angular/material/icon';
 import {MatBadgeModule} from '@angular/material/badge';
-
+import { TestErrorComponent } from './test-error/test-error.component';
+import { NotFoundComponent } from './not-found/not-found.component';
+import { ServerErrorComponent } from './server-error/server-error.component';
+import { ToastrModule } from 'ngx-toastr';
+import {MatSnackBarModule} from '@angular/material/snack-bar';
 
 
 @NgModule({
-  declarations: [NavBarComponent],
+  declarations: [NavBarComponent, TestErrorComponent, NotFoundComponent, ServerErrorComponent],
   imports: [
     CommonModule,
     RouterModule,
     MatToolbarModule,
     MatButtonModule,
     MatIconModule,
-    MatBadgeModule
+    MatBadgeModule,
+    // ToastrModule.forRoot({
+    //   positionClass: 'toast-bottom-right',
+    //   preventDuplicates: true
+    // }),
+    MatSnackBarModule
   ],
   exports: [NavBarComponent]
 })
---
+++ client/src/app/core/interceptors/error.interceptor.ts
@@ -0,0 +1,45 @@
+import { Router } from '@angular/router';
+import { Injectable } from '@angular/core';
+import { HttpInterceptor, HttpEvent, HttpHandler, HttpRequest } from '@angular/common/http';
+import { Observable, throwError } from 'rxjs';
+import { catchError } from 'rxjs/operators';
+import { ToastrService } from 'ngx-toastr';
+import { MatSnackBar } from '@angular/material/snack-bar';
+
+@Injectable()
+export class ErrorInterceptor implements HttpInterceptor {
+    constructor(private router: Router, private snackBar: MatSnackBar) {}
+
+    intercept(req: HttpRequest<any>, next: HttpHandler): Observable<HttpEvent<any>> {
+        return next.handle(req).pipe(
+            catchError(error => {
+                if (error) {
+
+                    if (error.status === 400) {
+                        if (error.error.errors) { // throw error back to component if its a validation error ie dont show a toast
+                            throw error.error;
+                        } else {
+                            this.snackBar.open(error.error.message, '', {
+                                duration: 5000,
+                            });
+                        }
+                    }
+
+                    if (error.status === 401) {
+                        this.snackBar.open(error.error.message, '', {
+                            duration: 5000,
+                        });
+                    }
+
+                    if (error.status === 404) {
+                        this.router.navigateByUrl('/not-found');
+                    }
+                    if (error.status === 500) {
+                        this.router.navigateByUrl('/server-error');
+                    }
+                }
+                return throwError(error);
+            })
+        );
+    }
+}
