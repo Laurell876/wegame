0b1b7bcd019e7789941778bc60eab69ebe2d2172
Laurell Seville <laurellseville2000@gmail.com>
2020-09-05 13:40:33 -0500

    end of section 11

--- client/src/app/core/interceptors/error.interceptor.ts
+++ client/src/app/core/interceptors/error.interceptor.ts
@@ -1,4 +1,4 @@
-import { Router } from '@angular/router';
+import { Router, NavigationExtras } from '@angular/router';
 import { Injectable } from '@angular/core';
 import { HttpInterceptor, HttpEvent, HttpHandler, HttpRequest } from '@angular/common/http';
 import { Observable, throwError } from 'rxjs';
@@ -14,9 +14,9 @@ export class ErrorInterceptor implements HttpInterceptor {
         return next.handle(req).pipe(
             catchError(error => {
                 if (error) {
-
+                    // throw error back to component if its a validation error ie dont show a toast
                     if (error.status === 400) {
-                        if (error.error.errors) { // throw error back to component if its a validation error ie dont show a toast
+                        if (error.error.errors) { 
                             throw error.error;
                         } else {
                             this.snackBar.open(error.error.message, '', {
@@ -35,7 +35,8 @@ export class ErrorInterceptor implements HttpInterceptor {
                         this.router.navigateByUrl('/not-found');
                     }
                     if (error.status === 500) {
-                        this.router.navigateByUrl('/server-error');
+                        const navigationExtras: NavigationExtras = { state: { error: error.error } };
+                        this.router.navigateByUrl('/server-error', navigationExtras);
                     }
                 }
                 return throwError(error);
