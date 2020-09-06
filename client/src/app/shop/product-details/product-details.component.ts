1e6027cec089e113791c54855d40379401110af3
Laurell Seville <laurellseville2000@gmail.com>
2020-09-06 17:12:49 -0500

    end of section 14

--- client/src/app/shop/product-details/product-details.component.ts
+++ client/src/app/shop/product-details/product-details.component.ts
@@ -1,3 +1,4 @@
+import { BasketService } from './../../basket/basket.service';
 import { Component, OnInit } from '@angular/core';
 import { IVideoGame } from 'src/app/Shared/Models/videogame';
 import { ShopService } from '../shop.service';
@@ -7,26 +8,48 @@ import { BreadcrumbService } from 'xng-breadcrumb';
 @Component({
   selector: 'app-product-details',
   templateUrl: './product-details.component.html',
-  styleUrls: ['./product-details.component.scss']
+  styleUrls: ['./product-details.component.scss'],
 })
 export class ProductDetailsComponent implements OnInit {
   videoGame: IVideoGame;
+  quantity = 1;
 
-  constructor( private shopService: ShopService, private activatedRoute: ActivatedRoute, private bcService: BreadcrumbService) { 
+  constructor(
+    private shopService: ShopService,
+    private activatedRoute: ActivatedRoute,
+    private bcService: BreadcrumbService,
+    private basketService: BasketService
+  ) {
     this.bcService.set('@videoGameDetails', '');
-   }
+  }
 
   ngOnInit(): void {
     this.loadProduct();
   }
 
-  loadProduct(): void {
-    this.shopService.getVideoGame(+this.activatedRoute.snapshot.paramMap.get('id')).subscribe(videoGame => {
-      this.videoGame = videoGame;
-      this.bcService.set('@videoGameDetails', videoGame.title);
-    }, error => {
-      console.log(error);
-    });
+  addItemToBasket() {
+    this.basketService.addItemToBasket(this.videoGame, this.quantity);
   }
 
+  changeQuantity(increment: boolean): void {
+    if (increment) {
+      this.quantity++;
+      return;
+    }
+    this.quantity--;
+  }
+
+  loadProduct(): void {
+    this.shopService
+      .getVideoGame(+this.activatedRoute.snapshot.paramMap.get('id'))
+      .subscribe(
+        (videoGame) => {
+          this.videoGame = videoGame;
+          this.bcService.set('@videoGameDetails', videoGame.title);
+        },
+        (error) => {
+          console.log(error);
+        }
+      );
+  }
 }
