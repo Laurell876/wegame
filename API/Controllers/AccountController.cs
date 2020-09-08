3cce3d2671c3a631e47923661e9f8c3ebc9c10fc
Laurell Seville <laurellseville2000@gmail.com>
2020-09-08 15:44:15 -0500

    end of section 16

--- API/Controllers/AccountController.cs
+++ API/Controllers/AccountController.cs
@@ -71,7 +71,7 @@ namespace API.Controllers
             var user = await _userManager.FindUserByClaimsPrincipalWithAddressAsync(HttpContext.User);
             user.Address = mapper.Map<AddressDto, Address>(address);
             var result = await _userManager.UpdateAsync(user);
-            
+
             if (result.Succeeded) return Ok(mapper.Map<Address, AddressDto>(user.Address));
 
             return BadRequest("Problem updating the user");
@@ -100,6 +100,12 @@ namespace API.Controllers
         [HttpPost("register")]
         public async Task<ActionResult<UserDto>> Register(RegisterDto registerDto)
         {
+            if (CheckEmailExistsAsync(registerDto.Email).Result.Value)
+            {
+                return new BadRequestObjectResult(new ApiValidationErrorResponse{Errors = new [] {"Email address is in use"}});
+            }
+
+
             var user = new AppUser
             {
                 DisplayName = registerDto.DisplayName,
